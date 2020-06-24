5cb73888c6f6fb41305ccf424b80bc59a82af69b
isabelwu6464 <isabelwu@college.harvard.edu>
2020-06-24 16:27:58 -0400

    Updated implementation of byline and source extraction

--- src/python_scripts/article-processing.py
+++ src/python_scripts/article-processing.py
@@ -1,70 +1,115 @@
 from __future__ import print_function
 from newspaper import Article
+from genderize import Genderize
 import nltk
 from nltk.tokenize import sent_tokenize
-from nltk.tag import StanfordNERTagger
 import gender_guesser.detector as gender
 import spacy 
 import numpy as np
 import json
 import os
 import itertools
+import string
 import urllib
+import requests, mediacloud.api
+from collect_articles import article_urls
+from bs4 import BeautifulSoup
+import re
 
 def main():
-    URL = "https://www.nbcnews.com/news/us-news/how-police-killing-rayshard-brooks-could-empower-atlanta-s-citizen-n1231204"
-    #nltk.download('punkt')
-    gen_detect = gender.Detector() #Initialize Gender Detector
-    
-    article = Article(URL)
-    article.download()
-    article.parse()
-    main_body = article.text #Main body of Article
-    attempted_authors = article.authors
-    authors = author_names(attempted_authors) #Returns array of Authors
-    #gender_of_authors = gender_author(authors)
-    gender_of_author = gen_detect.get_gender(authors.split()[0])
-    count_he = main_body.count(" he ") #Count number of hes
-    count_she = main_body.count(" she ") #Count number of shes
-    
-    sentences = sent_tokenize(main_body)
-    sources = extract_sources(sentences)
-    print(article.authors)
-    print(article.publish_date)
-    print(article.text)
+    urls = article_urls(0, 1000, 39000)
+    for URL in urls:
+        #URL = 'https://abcnews.go.com/Politics/white-house-odd-couple-trump-boltons-tumultuous-relationship/story?id=71323127'
+        article = Article(URL)
+        article.download()
+        article.parse()
+        main_body = article.text #Main body of Article
+        attempted_authors = article.authors
 
-    #Write to JSON
-    # Data to be written 
-    x = type(gender_of_author)
-    y = type(sources.tostring())
-    dictionary ={ 
-        "title" : article.title, 
-        "Author" : authors, 
-        "Gender of Author": gender_of_author,
-        "Sources": np.array_str(sources),
-        "# of \'he\'s" : count_he, 
-        "# of \'she\'s" : count_she
-    } 
-  
-    # Serializing json  
-    json_object = json.dumps(dictionary, indent = 4) 
-  
-    # Writing to sample.json 
-    with open("example.json", "w") as outfile: 
-        outfile.write(json_object) 
+        #HTML Processing
+        fp = urllib.request.urlopen(URL)
+        mybytes = fp.read()
 
-    #article.nlp()
-    #print(article.keywords)
-    #print(article.summary)
+        mystr = mybytes.decode("utf8")
+        fp.close()
+        soup = BeautifulSoup(mystr, 'lxml')
+        author = soup.find("div", class_="Byline__Author").text #Byline through HTML 
+
+        authors = author_names(attempted_authors) #Returns array of Authors through NER
+        gender_of_author = Genderize().get([authors.split()[0]])
+        sentences = sent_tokenize(main_body)
+        sources2 = extract_sources(sentences)
+        sources= extract_sources_displacy(sentences)
+        source_genders = source_gender(sources)
+        
+        count_he = source_genders.count('male')
+        count_she = source_genders.count('female')
+      
+        #Write to JSON
+        # Data to be written
+        dictionary ={ 
+            "title" : article.title, 
+            "Author" : authors, 
+            "Gender of Author": gender_of_author,
+            "Sources": np.array_str(sources),
+            "# of male sources" : count_he, 
+            "# of female sources" : count_she
+        } 
+    
+        # Serializing json  
+        json_object = json.dumps(dictionary, indent = 4) 
+    
+        # Writing to sample.json 
+        with open("example.json", "w") as outfile: 
+            outfile.write(json_object) 
+
 def extract_sources_stanford(sentences):
     java_path = r"C:\Program Files (x86)\Java\jre1.8.0_251\bin\java.exe"
     os.environ['JAVAHOME'] = java_path
     st = StanfordNERTagger('C:/Users/isabe/Downloads/stanford-ner-4.0.0/stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz', 'C:/Users/isabe/Downloads/stanford-ner-4.0.0/stanford-ner-4.0.0/stanford-ner-4.0.0.jar')
     split_text = st.tag(sentence.split()) 
+
+def extract_sources_displacy(sentences):
+    nlp = spacy.load('en_core_web_sm') #Load english
+    sources = []
+    for i, sentence in enumerate(sentences):
+        quote_words_bool = ("said" or "according to" or "saying" or "say" or "says") in sentence
+        sen = nlp(sentence)
+        if quote_words_bool:
+            #Merge noun chunks
+            #for noun_phrase in list(sen.noun_chunks):
+                #noun_phrase.merge(noun_phrase.root.tag_,noun_phrase.root.lemma_, noun_phrase.root.ent_type_)
+            #Find verb signifier
+            for token in sen:
+                if token.text == "said" or "according to " or "saying" or "say" or "says":
+                    for a in token.children:
+                        if a.dep_ == 'nsubj' and a.pos_ == 'PROPN':
+                            sources.append(a.text)
+    sources = np.array(sources)
+    sources = np.unique(sources) #Remove duplicates
+
+    #Remove non-names
+    for name in sources[:]:
+        if name.islower() or any(p in name for p in string.punctuation):
+            sources = np.delete(sources, np.where(sources == name))
+
+    #Find full names
+    sources_full_name = []
+    for source in sources:      
+        for sent in sentences:
+            split_sent = re.findall(r"[\w']+|[.,!?;]", sent)
+            if source in split_sent:
+                #split_sent = sent.split() #Split sentence into list
+                index = split_sent.index(source)
+                sources_full_name.append(split_sent[index - 1] + " " + split_sent[index])
+                break
+    sources_full_name = np.array(sources_full_name)
+    return sources_full_name 
+           
+
 def extract_sources(sentences):
     nlp = spacy.load('en_core_web_sm') #Load english in space  
     sentences_with_people = []
-
     for i, sentence in enumerate(sentences):
         
         sen = nlp(sentence)
@@ -79,36 +124,36 @@ def extract_sources(sentences):
                 sentences_with_people.append([sentence, ent.text])
                 person_found = True
             k = k + 1
-    sentences_with_people_np = np.array(sentences_with_people)
-    sources = np.unique(sentences_with_people_np[:,1])
+    sources = np.array(sentences_with_people)
+
+    #Check for empty array
+    if len(sources) == 0:
+        return sources
+    
+    sources = np.unique(sources[:,1]) #Remove repeats
 
     #Remove Repeat Last Names
     for name in sources[:]:
         x = len(name.split())
         if len(name.split()) == 1:
-            #sources.remove(name)
             sources = np.delete(sources, np.where(sources == name))
 
     return sources
     
-def gender_author(authors):
-    #api_key = open('AIzaSyBv4a_fpRuAaNuqj46qoyNJye4JXs6HNm0.api_key').read()
-    api_key = 'AIzaSyBv4a_fpRuAaNuqj46qoyNJye4JXs6HNm0'
-    query = 'Anna Bahney'
-    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
-    params = {
-        'query': query,
-        'limit': 10,
-        'indent': True,
-        'key': api_key,
-    }
-    url = service_url + '?' + urllib.parse.urlencode(params)
-    response = json.loads(urllib.request.urlopen(url).read())
-    for element in response['itemListElement']:
-        x = element
-        print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
-        #print(element['result']['gender'])
-    
+def source_gender(sources):
+    #Check for no identified sources
+    if len(sources) == 0:
+        genders = ['None']
+    else:
+        source_first_name = []
+        for source in sources:
+            source_first_name.append(source.split()[0])
+        source_genders = Genderize().get([source_first_name])
+        genders =[]
+        for d in source_genders:
+            genders.append(d['gender'])
+
+    return genders
 def author_names(attempted_authors):
     entities = []
     sp = spacy.load('en_core_web_sm') #Load english in spacy
